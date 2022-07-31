f7c8c67b39593698ead64428b451e680aa3c2e01
Jean Carlo Jimenez <jeanjimenez143@gmail.com>
2022-07-31 15:48:06 -0600

    Add user repository

---
+++ ToDoLIst_DAL/Contracts/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ToDoLIst_DAL.Entities;
+
+namespace ToDoLIst_DAL.Contracts
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<IdentityError>> AddAsync(AppUser newUser, string userPassword);
+        Task<AppUser?> GetAsync(string email);
+        Task<IEnumerable<IdentityError>> UpdateAsync(AppUser user);
+        Task<IEnumerable<IdentityError>> UpdatePasswordAsync(AppUser user, string oldPassword, string newPassword);
+        Task DeleteAsync(AppUser user);
+
+        Task<bool> CheckPasswordAsync(AppUser user, string password);
+    }
+}
---
+++ ToDoLIst_DAL/Repositories/UserRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ToDoLIst_DAL.Contracts;
+using ToDoLIst_DAL.Entities;
+
+namespace ToDoLIst_DAL.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserRepository(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public Task DeleteAsync(AppUser user)
+        {
+            return _userManager.DeleteAsync(user);
+        }
+
+        public async Task<AppUser?> GetAsync(string email)
+        {
+            return await _userManager.FindByEmailAsync(email);
+        }
+
+        public async Task<IEnumerable<IdentityError>> AddAsync(AppUser newUser, string userPassword)
+        {
+            var result = await _userManager.CreateAsync(newUser, userPassword);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, "User");
+            }
+
+            return result.Errors;
+        }
+
+        public async Task<IEnumerable<IdentityError>> UpdateAsync(AppUser user)
+        {
+            var result = await _userManager.UpdateAsync(user);
+            return result.Errors;
+        }
+
+        public async Task<IEnumerable<IdentityError>> UpdatePasswordAsync(AppUser user, string oldPassword, string newPassword)
+        {
+            var result = await _userManager.ChangePasswordAsync(user, oldPassword, newPassword);
+            return result.Errors;
+        }
+
+        public async Task<bool> CheckPasswordAsync(AppUser user, string password)
+        {
+            return await _userManager.CheckPasswordAsync(user, password);
+        }
+    }
+}
